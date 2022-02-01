31818745ab1b6ab8ae509530aab1f177aaca92f4
C Bares <colinedwinbares@gmail.com>
2022-02-01 13:56:16 -0600

    setting up post route

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+const { notes } = require('./db/db.json');
+const fs = require('fs');
+const { v4: uuidv4 } = require("uuid");
+
 const express = require("express");
 const app = express();
 
@@ -5,6 +9,7 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
@@ -15,6 +20,18 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+})
+
+app.post('/api/notes', (req, res) => {
+  let note = req.body;
+  note.id = uuidv4();
+  res.json(note);
+})
+
 app.listen(PORT, () => {
-  console.log("Listening");
+  console.log("Listening on " + PORT);
 });
+
+console.log(notes);
